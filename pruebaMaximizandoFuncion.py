ceb9e390d408d12c2d9ecfee74398a0bfba75402
dieguito3076 <diegovelazqu@hotmail.com>
2019-01-31 23:27:07 -0600

    Maximizando punto en funciones

--- pruebaMaximizandoFuncion.py
+++ pruebaMaximizandoFuncion.py
@@ -19,16 +19,11 @@ def crear_poblacion(numero_de_poblacion_inicial, len_elemento):
     return poblacion
 
 
-poblacion = crear_poblacion(5, 3)
-AG = Genetic_Algorithm(poblacion, '101', 0.01, 0.3,'cross_over_probabilistic')
+poblacion = crear_poblacion(6, 3)
+print poblacion
+#AG = Genetic_Algorithm(poblacion, '101', 0.01, 0.3,'cross_over_probabilistic')
 
 
-print AG.population
-AG.evolve()
-print AG.population
-
-matriz_Y = np.array([[1],[2],[3],[4]])
-matriz_res = np.array([[0,7],[0.9],[0.89],[0.91]])
-
-res = matriz_Y - matriz_res
-print res
+#print AG.population
+#AG.evolve()
+#print AG.population
